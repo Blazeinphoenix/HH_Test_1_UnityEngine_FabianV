253bfed3b671f5c72bd1c42d77c45bba0d0056d7
Blazeinphoenix <BlazeingphoenixHD@gmail.com>
2024-02-21 12:07:31 +0100

    UI Stuff

---
+++ Assets/Scenes/Save.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Save : MonoBehaviour
+{
+    public TMP_InputField inputText;
+    
+    private string tutorialText;
+
+  
+
+    public void SaveThis()
+    {
+        tutorialText = inputText.text;
+        PlayerPrefs.SetString("tutorialTextKeyName", tutorialText);
+    }
+
+    public void LoadThis()
+    {
+
+        inputText.text = PlayerPrefs.GetString("tutorialTextKeyName", "");
+
+    }
+
+    public void DeleteThis()
+    {
+        PlayerPrefs.DeleteAll();
+        inputText.text = "";
+
+    }
+
+    public void ClearThis()
+    {
+       
+        inputText.text = "";
+
+    }
+}
